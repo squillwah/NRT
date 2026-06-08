105229b769197ff6995f1f1641fbc218df177a05
squillwah <Swagapotato@gmail.com>
2026-06-07 20:07:21 -0400

    Search for articles and download pdfs
    Uses eutil search and fetch, attempts to retrieve article json from PMC BioC, pulls info from PMC OA, prompts pdf download if available in FTP.

--- scripts/pmc_oa_rest_test.py
+++ scripts/pmc_oa_rest_test.py
@@ -1,25 +1,39 @@
 import requests
 import json
-#import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET     use this over helper for more complex stuff with xml 
+#from ftplib import FTP
+import urllib.request
 
-BASE_URLS = {"UTIL":"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/",                 # PMC Database Search
-            "OA":"https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi",                # Open Access Subset DB
-            "OAI":"https://pmc.ncbi.nlm.nih.gov/api/oai/v1/mh/",                     # OA Subset Metadata DB
-            "BIOC":"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi", # OA Subset (BioC Format) DB
-            "CITE":"https://pmc.ncbi.nlm.nih.gov/api/ctxp/",                         # Citation Exporter
-            "ID":"https://pmc.ncbi.nlm.nih.gov/tools/idconv/api/v1/articles/"}       # PMC ID Converter (PMCID, PMID, DOI, ...)
+# API base URLs
+PMC = {"OA":  "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi",              # Open Access Subset DB
+       "OAI": "https://pmc.ncbi.nlm.nih.gov/api/oai/v1/mh/",                    # OA Subset Metadata DB
+       "BIOC":"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi", # OA Subset (BioC Format) DB
+       "CITE":"https://pmc.ncbi.nlm.nih.gov/api/ctxp/",                         # Citation Exporter
+       "ID":  "https://pmc.ncbi.nlm.nih.gov/tools/idconv/api/v1/articles/"}     # PMC ID Converter (PMCID, PMID, DOI, ...)
 
-DB_FETCH_URL = BASE_URLS["UTIL"]+"efetch.fcgi"
-DB_SEARCH_URL = BASE_URLS["UTIL"]+"esearch.fcgi"
+EUTIL = {"SEARCH":"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi",     # PMC Database Search
+         "FETCH":"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"}
 
+# Helpers
+def fail_check(condition, msg):
+    if condition:
+        print(msg)
+        quit()
 
-#payload = {"db":"pmc", "term":"cancer"} # Pretty sure pmc means all OA papers, but not sure.
-#response = requests.get(DB_SEARCH_URL, params=payload)
-#print(response.url)
-#print(response.text)
-#print(response.headers)
-#print("\n\n\n", "\n".join(vars(response)), "\n\n\n")
-#print(response.content)
+def xml_extract_first(delims, xml):
+    value = None
+    value_start = xml.find(delims[0]) + len(delims[0])
+    value_end = xml.find(delims[1], value_start)
+    if value_end > value_start and value_start >= len(delims[0]) : value = xml[value_start:value_end]
+    return value
+
+def download_pmc_ftp(url, filename):
+    insert_deprecated = url.find("pmc/")+4  # PMC moved all rearranged their FTP files into a "/deprecated/" subfolder. Without updating the database URLs :)
+    url = url[0:insert_deprecated]+"deprecated/"+url[insert_deprecated:]
+    urllib.request.urlretrieve(url, filename)
+
+search_term = str(input("Enter search term: "))
+print(f"Searching PMC database for OA papers on {search_term}...")
 
 article = None
 article_id = 0
@@ -29,47 +43,53 @@ response = None
 response_data = None
 
 # Search PMC database for papers on "cancer"
-payload = {"db":"pmc",
-           "sort":"pub date",
-           "format":"json",     # Some other params could probably go here, filtering etc. Does PMC contain only OA papers?
-           "term":"Cancer AND open_access[filter] AND free_full_text[filter]"}
-response = requests.get(DB_SEARCH_URL, params=payload)
-if (response.status_code != 200):
-    print(f"Failed to GET {response.url} with code {response.status_code}")
-    quit()
+print("\n Searching PMC")
+#payload = {"db":"pmc", "sort":"pub date", "format":"json", "term":f"{search_term} AND open_access[filter]"} # Some other params could probably go here, filtering etc. Does PMC contain non OA papers or is that only PubMed?
+payload = {"db":"pmc", "sort":"relevance", "format":"json", "term":search_term} # filter=collections.open_access&schema=all
+response = requests.get(EUTIL["SEARCH"], params=payload)
+fail_check((response.status_code != 200), f"! Failed to GET {response.url} with code {response.status_code}")
 response_data = response.json()
-database_uid = response_data["esearchresult"]["idlist"][1] # First result
-print(f"Found article in PMC database with UID '{database_uid}'")
+fail_check((len(response_data["esearchresult"]["idlist"]) == 0), "! No articles found")
+database_uid = response_data["esearchresult"]["idlist"][0] # First result
 print(response.url)
+print(f"Selecting article in PMC database with UID '{database_uid}' out of querried list: {response_data["esearchresult"]["idlist"]}")
 
-# Fetch article info from database, map UID to PMCID        * This step might be completely unnecessary, not sure.
-payload = {"db":"pmc",
-           "id":database_uid}
-response = requests.get(DB_FETCH_URL, params=payload)
-if (response.status_code != 200):
-    print(f"Failed to GET {response.url} with code {response.status_code}")
-    quit()
-response_data = response.text # XML parsing 
-xml_pmcid_delims = ('<article-id pub-id-type="pmcid">', '</article-id>')
-xml_pmcid_indices = [response_data.find(xml_pmcid_delims[0]), response_data.find(xml_pmcid_delims[1])]
-xml_pmcid_indices[0] = xml_pmcid_indices[0] + len(xml_pmcid_delims[0])
-article_id = response_data[xml_pmcid_indices[0]:xml_pmcid_indices[1]]
-print(f"Mapped database UID '{database_uid}' to PMCID '{article_id}'")
+# Fetch article info from database, map UID to PMCID        * This step might be completely unnecessary, not sure (is the PMCID always PMC+UID?)
+print("\n Mapping UID to PMCID")
+payload = {"db":"pmc", "id":database_uid}
+response = requests.get(EUTIL["FETCH"], params=payload)
+fail_check((response.status_code != 200), f"! Failed to GET {response.url} with code {response.status_code}")
+response_data = response.text
+article_id = xml_extract_first(('<article-id pub-id-type="pmcid">', '</article-id>'), response_data) # Parse PMCID from XML text
 print(response.url)
+print(f"Mapped database UID '{database_uid}' to PMCID '{article_id}'")
 
 # Get DoiC formatted copy of article, as unicode JSON.
-response = requests.get(f"{BASE_URLS["BIOC"]}/BioC_json/{article_id}/unicode")
-if (response.status_code != 200):
-    print(f"Failed to GET {response.url} with code {response.status_code}")
-    quit()
-print(response.url)
-print(response.content)
-response_data = response.json()
-print(json.dumps(response_data, indent=4))
-    #article_id = response_data[3][0].text # idList is the fourth child of XML root.
-
+print("\n Requesting DoiC JSON")
+response = requests.get(f"{PMC["BIOC"]}/BioC_json/{article_id}/unicode")
+fail_check((response.status_code != 200), f"! Failed to GET {response.url} with code {response.status_code}")
+try:
+    response_data = response.json()
+    print(f"DoiC JSON of {article_id}:\n {json.dumps(response_data, indent=4)}")
+    print(response.url)
+except:
+    print(f"! Response from {response.url} is not in JSON format. Probably not found.")
+    #print(response.content)
 # Keep getting not found errors with the DoiC database. 
-# I think it just doesn't have all the papers indexed in that format, and I can't find a way to restrict the search. 
-# Not like that really matters tho, cause it's only testing.
+# Probably, not all papers are indexed in that format. I can't find a way to restrict the search to those that are.
+# PMID vs PMCID? Sometimes articles are in DoiC, but only under one of the two IDs. Could use PMC ID API to translate.
 
+# Get info from standard OA API
+print("\n Requesting standard PMC info")
+payload = {"id":article_id} # Does not support json :(
+response = requests.get(PMC["OA"], params=payload)
+fail_check((response.status_code != 200), f"! Failed to GET {response.url} with code {response.status_code}")
+print(response.text)
+if (pdf_url := xml_extract_first(('<link format="pdf"', "/>"), response.text)) != None:
+    pdf_url = pdf_url[pdf_url.find("href=")+5:].strip()[1:-1]
+    print(f"\nPDF: {pdf_url}")
+    if input("Download? (y/n): ").strip().lower() == "y":
+        print("...")
+        download_pmc_ftp(pdf_url, article_id+".pdf")
+        print("Done!")
 
