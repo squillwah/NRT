d8717cbbc7f9446c7aadedb9a1348e1b8a96bc91
squillwah <Swagapotato@gmail.com>
2026-06-13 16:26:30 -0400

    Write references to file.
    Plus sloppy patch to use pmcids in reference exporter (not all pmc papers found had pubmid IDs).

--- scripts/reference_db/source_references.py
+++ scripts/reference_db/source_references.py
@@ -12,22 +12,26 @@ import json
 import xml.etree.ElementTree as ET
 import time
 
-# "When using this API programmatically, we request that you limit your application's rate to 3 requests per second and do not make concurrent requests to this service, even at off-peak times. Any site (IP address) posting more than 3 requests per second to the service will receive an error message. Use the tool and email parameters to identify the application making the request."
+# ! Note: none of these functions handle unexpected responses. They will throw errors with bad IDs, search terms, dates, etc.
+
+# "When using this API programmatically, we request that you limit your application's rate to 3 requests per second and 
+# do not make concurrent requests to this service, even at off-peak times. Any site (IP address) posting more than 3 requests 
+# per second to the service will receive an error message. Use the tool and email parameters to identify the application making the request."
 REQUEST_DELAY = .34
 REQUEST_IDENTIFIER_TOOL = "reference_scraper"
 REQUEST_IDENTIFIER_EMAIL = "DRE44769@pennwest.edu"
-ENDPOINT_ESEARCH = ""
+ENDPOINT_ESEARCH = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
 ENDPOINT_OASERVICE = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi"
-ENDPOINT_CITATIONS = "https://pmc.ncbi.nlm.nih.gov/api/ctxp/v1/pubmed/"
+#ENDPOINT_CITATIONS = "https://pmc.ncbi.nlm.nih.gov/api/ctxp/v1/pubmed/"
+ENDPOINT_CITATIONS = "https://pmc.ncbi.nlm.nih.gov/api/ctxp/v1/pmc/"
 ENDPOINT_IDCONVERTER = "https://pmc.ncbi.nlm.nih.gov/tools/idconv/api/v1/articles/"
 
 def kindly_get(url, params):
     time.sleep(REQUEST_DELAY)
     return requests.get(url, params=({"tool":REQUEST_IDENTIFIER_TOOL, "email":REQUEST_IDENTIFIER_EMAIL} | params))
 
-# ! Note: none of these handle unexpected responses. They will throw errors.
-
 # Returns a list of JSON/dicts containing DOIs, PMCIDs and PMIDs for each given PMCID.
+# ----
 def all_ids(*pmcids):
     MAX_IDS = 200   # The API service allows for up to 200 IDs in a single request.
     id_lists = []
@@ -37,31 +41,58 @@ def all_ids(*pmcids):
     id_lists = kindly_get(ENDPOINT_IDCONVERTER, params={"format":"json", "ids":",".join(pmcids)}).json()["records"] + id_lists # Insert at head to maintain parallel order with arguments
     return id_lists
 
-# Returns a # of PMCIDs within OA subset corresponding to given search term.
-def get_papers_filter(count, terms): pass
+# Returns PMCIDs within OA subset corresponding to given search term.
+# ----
+def get_papers_filter(count, terms):
+    # Max of 100,000 for one search request
+    request = kindly_get(ENDPOINT_ESEARCH, params={"db":"pmc", "sort":"pubdate", "format":"json", "term":f"open_access[Filter]{terms}", "retmax":count})
+    #return ["PMC"+ID for ID in request.json()["esearchresult"]["idlist"]]
+    return request.json()["esearchresult"]["idlist"]
 
-# Returns a # of PMCIDs updated in OA database within date range (not the same as publication range).
+# Returns PMCIDs updated in OA database within date range (not the same as publication range).
+# ----
 def get_papers_range(count, start, end):
-    # ! rn only gets first 1000 max, will need to use resumptiontoken for more later
+    # ! Only retrieves first 1000 at maximum. Will need to use resumptiontoken for more.
     request = kindly_get(ENDPOINT_OASERVICE, params={"from":start, "until":end})
-    print(request.url)
     xml = ET.fromstring(request.text)
     return [record.attrib["id"] for record in xml.find("records").findall("record")[:count]]
 
 # Returns a JSON/dict of nested dicts containing all formal citation formats for each given paper.
-def extract_reference_formats(*pmids):
-    # May have some maximum, it is unclear.
-    refs = kindly_get(ENDPOINT_CITATIONS, params={"format":"citation", "id":pmids}).json() # JSON of AMA, APA, MLA, & NLM for each PMCID
-    riss = [ris+"ER  - \r\n" for ris in kindly_get(ENDPOINT_CITATIONS, params={"format":"ris", "id":pmids}).text.split("ER  - ")] # List of RIS metadata text
+# ! Note: needs PMIDs, not PMCIDS. Set the flag to attempt auto translation via all_ids().
+# ----
+def extract_reference_formats(*pmcids, translate_IDs=False):
+    #if translate_IDs: pmids = [ID["pmid"] for ID in all_ids(*pmids)]
+    # ! Requests may have some maximum, documentation is unclear.
+    refs = kindly_get(ENDPOINT_CITATIONS, params={"format":"citation", "id":pmcids}).json() # JSON of AMA, APA, MLA, & NLM for each PMCID
+    riss = [ris+"ER  - \r\n" for ris in kindly_get(ENDPOINT_CITATIONS, params={"format":"ris", "id":pmcids}).text.split("ER  - ")] # List of RIS metadata text
     for paper_refs, paper_ris in zip(refs, riss): paper_refs["ris"] = paper_ris # Include RIS as entry within refs JSON
     return refs
 
-#print(json.dumps(all_ids("PMC1193645", "PMC1134901"), indent=2))
-#print(json.dumps(all_ids("PMC1134901", "PMC1193645"), indent=2))
+# @todo: Something to parse the RIS metadata. If using that is something we're going to include as an experiment.
 
-paper_IDs = all_ids(*get_papers_range(2, "2026-01-01", "2026-04-01"))
-paper_refs = extract_reference_formats(*[ID["pmid"] for ID in paper_IDs])
-print(json.dumps(paper_IDs, indent=2))
-print(json.dumps(paper_refs, indent=2))
+if __name__ == "__main__":
+    print("Searching papers.")
+    paper_pmcids = get_papers_filter(100, "BMJ[Journal]") # Could do multiple journals in a search, or grab an exact amount from each in seperate calls for equal ratios. And more.
+    print(paper_pmcids)
+
+    print("Mapping IDs")
+    #paper_IDs = all_ids(*paper_pmcids)
+    #print(json.dumps(paper_IDs, indent=2))
+
+    print("Fetching references")           # or (*paper_pmcids, translate_IDs=True)
+    #paper_references = extract_reference_formats([ID["pmid"] for ID in paper_IDs])
+    paper_references = extract_reference_formats(*paper_pmcids)
+    #print(json.dumps(paper_references, indent=2))
+
+    print("Writing to file")
+    try:
+        with open("references.json", "x") as file: json.dump(paper_references, file, indent=2)
+        print("Saved to 'references.json'")
+    except FileExistsError:
+        if input("! 'references.json' already exists, overwrite? [y/n]: ").strip().lower() == "y":
+            with open("references.json", "w") as file: json.dump(paper_references, file, indent=2)
+            print("Saved to 'references.json'")
+        else: print("Write cancelled")
+
 
 
