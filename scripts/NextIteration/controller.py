5ebc829bdd45cceb1a525bc9e9f6cdb44d480a78
Ryan Hayden <ryanhpersonalemail@gmail.com>
2026-06-15 11:53:56 -0400

    Added controller.py & references.json

--- scripts/NextIteration/controller.py
+++ scripts/NextIteration/controller.py
@@ -1,6 +1,6 @@
 import json
 
-from NRT.scripts.NextIteration.prompt_writer import generate_prompt
+from prompt_writer import generate_prompt
 
 
 parsed_refs = {}
@@ -10,11 +10,16 @@ name = input("Please enter the json file name with the .json extension: ")
 with open(name, 'r') as file:
     parsed_refs = json.load(file)
 
-print(parsed_refs[2]["apa"]["orig"])
+print(parsed_refs[0])
 
 header_prompt = generate_prompt()
 
 print("done")
+#for i in parsed_refs:
+    #jsonObject = method(String HeaderPrompt, String individualCitation)
+
+
+
 '''
 response = Gemini.send_prompt("gemma-4-26b-a4b-it", header_prompt + )
 print("\nThe response is: \n\n", Gemini.parse_response(response))'''
