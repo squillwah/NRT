8dca38e130395e9008dedfb4bd8142d267c39ea9
squillwah <Swagapotato@gmail.com>
2026-06-17 14:55:42 -0400

    Exclude corrections, concerns, retractions

--- reference_dataset/source_refs.py
+++ reference_dataset/source_refs.py
@@ -32,41 +32,44 @@ def kindly_get(url, params):
 
 # Returns a list of JSON/dicts containing DOIs, PMCIDs and PMIDs for each given PMCID.
 # ----
-def all_ids(*pmcids):
-    MAX_IDS = 200   # The API service allows for up to 200 IDs in a single request.
-    id_lists = []
-    if isinstance(pmcids, tuple) and len(pmcids) > MAX_IDS:
-        id_lists = all_ids(*pmcids[MAX_IDS:])
-        pmcids = pmcids[0:MAX_IDS]
-    id_lists = kindly_get(ENDPOINT_IDCONVERTER, params={"format":"json", "ids":",".join(pmcids)}).json()["records"] + id_lists # Insert at head to maintain parallel order with arguments
-    return id_lists
+#def all_ids(*pmcids):
+#    MAX_IDS = 200   # The API service allows for up to 200 IDs in a single request.
+#    id_lists = []
+#    if isinstance(pmcids, tuple) and len(pmcids) > MAX_IDS:
+#        id_lists = all_ids(*pmcids[MAX_IDS:])
+#        pmcids = pmcids[0:MAX_IDS]
+#    id_lists = kindly_get(ENDPOINT_IDCONVERTER, params={"format":"json", "ids":",".join(pmcids)}).json()["records"] + id_lists # Insert at head to maintain parallel order with arguments
+#    return id_lists
 
 # Returns PMCIDs within OA subset corresponding to given search term.
 # ----
 def get_papers_filter(count, terms):
     # Max of 100,000 for one search request
-    request = kindly_get(ENDPOINT_ESEARCH, params={"db":"pmc", "sort":"pubdate", "format":"json", "term":f"open_access[Filter]{terms}", "retmax":count})
+    EXCLUDE = ("articletypeexpressionofconcern", "articletypecorrection", "articletyperetraction")
+    request = kindly_get(ENDPOINT_ESEARCH, params={
+        "db":"pmc", "sort":"pubdate", "format":"json",
+        "term":f"open_access[Filter]{terms} NOT ({" OR ".join(EXCLUDE)})",
+        "retmax":count})
     #return ["PMC"+ID for ID in request.json()["esearchresult"]["idlist"]]
     return request.json()["esearchresult"]["idlist"]
 
 # Returns PMCIDs updated in OA database within date range (not the same as publication range).
 # ----
-def get_papers_range(count, start, end):
-    # ! Only retrieves first 1000 at maximum. Will need to use resumptiontoken for more.
-    request = kindly_get(ENDPOINT_OASERVICE, params={"from":start, "until":end})
-    xml = ET.fromstring(request.text)
-    return [record.attrib["id"] for record in xml.find("records").findall("record")[:count]]
+#def get_papers_range(count, start, end):
+#    # ! Only retrieves first 1000 at maximum. Will need to use resumptiontoken for more.
+#    request = kindly_get(ENDPOINT_OASERVICE, params={"from":start, "until":end})
+#    xml = ET.fromstring(request.text)
+#    return [record.attrib["id"] for record in xml.find("records").findall("record")[:count]]
 
 # Returns a JSON/dict of nested dicts containing all formal citation formats for each given paper.
-# ! Note: needs PMIDs, not PMCIDS. Set the flag to attempt auto translation via all_ids().
+# ! Note: needs PMIDs, not PMCIDS. Set the flag to attempt auto translation via all_ids(). ! Note: no longer true
 # ----
-def extract_reference_formats(*pmcids, translate_IDs=False):
+def extract_reference_formats(*pmcids): #(*pmids, translate_IDs=False):
     #if translate_IDs: pmids = [ID["pmid"] for ID in all_ids(*pmids)]
     # ! Requests may have some maximum, documentation is unclear.
-    RIS_DELIM = "ER - \r\n"
+    RIS_DELIM = "ER  - \r\n"
     refs = kindly_get(ENDPOINT_CITATIONS, params={"format":"citation", "id":pmcids}).json() # JSON of AMA, APA, MLA, & NLM for each PMCID
     riss = [ris+RIS_DELIM for ris in kindly_get(ENDPOINT_CITATIONS, params={"format":"ris", "id":pmcids}).text.split(RIS_DELIM)] # List of RIS metadata text
-    #with open("fart.txt", "w") as fart: fart.write(kindly_get(ENDPOINT_CITATIONS, params={"format":"ris", "id":pmcids}).text)
     for paper_refs, paper_ris in zip(refs, riss): paper_refs["ris"] = paper_ris # Include RIS as entry within refs JSON
     return refs
 
@@ -77,14 +80,13 @@ if __name__ == "__main__":
     paper_pmcids = get_papers_filter(100, "Nature[Journal]") # Could do multiple journals in a search, or grab an exact amount from each in seperate calls for equal ratios. And more.
     print(paper_pmcids)
 
-    print("Mapping IDs")
+    # Made redundant by change to PMC db from PubMed db in reference API
+    #print("Mapping IDs")
     #paper_IDs = all_ids(*paper_pmcids)
-    #print(json.dumps(paper_IDs, indent=2))
 
     print("Fetching references")           # or (*paper_pmcids, translate_IDs=True)
     #paper_references = extract_reference_formats([ID["pmid"] for ID in paper_IDs])
     paper_references = extract_reference_formats(*paper_pmcids)
-    #print(json.dumps(paper_references, indent=2))
 
     print("Writing to file")
     try:
