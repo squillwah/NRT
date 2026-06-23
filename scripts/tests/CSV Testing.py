23f259ed80087f828d8f97ff98d4a6ec6af07e01
Majamaster10 <156863772+Majamaster10@users.noreply.github.com>
2026-06-23 11:47:53 -0400

    Created the method to write the results to a xlsx, takes in a list of dicts and puts them into a xlsx

--- scripts/tests/CSV Testing.py
+++ scripts/tests/CSV Testing.py
@@ -1,11 +1,47 @@
 import csv
+import openpyxl
+from openpyxl import Workbook
 
-data = [
-    ["Name", "Age", "City"],
-    ["Alice", 30, "New York"],
-    ["Bob", 25, "London"],
-]
+def write_csv(inputted_dict):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "Output from Models"
 
-with open("output.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(data)  # Write all rows at once
+    for i in range(len(inputted_dict)):
+        new_sheet = wb.create_sheet(title="Test" + str(i))
+        headers = ["Overall", "Author", "Journal", "Publish Date", "Author Order", "Publisher", "Percentage of Confidence", "Model"]
+
+        new_sheet.append(headers)
+        for item in inputted_dict:
+            row_values = [item[key] for key in headers]
+            new_sheet.append(row_values)
+
+    wb.save(filename="output.xlsx")
+
+
+
+
+# data = [
+#     {"Overall": "Fake", "Author": "Real", "Journal": "Real", "Publish Date": "Fake", "Author Order": "Real", "Publisher": "Real", "Percentage of Confidence": 95.00001, "Model": "Chat-GPT"},
+#     {"Overall": "Fake", "Author": "Real", "Journal": "Real", "Publish Date": "Fake", "Author Order": "Real", "Publisher": "Real", "Percentage of Confidence": 95.00001, "Model": "Gemini"}
+# ]
+#
+# wb = openpyxl.Workbook()
+# ws = wb.active
+# ws.title = "Test 1"
+#
+# for i in range(len(data)):
+#     new_sheet = wb.create_sheet(title="Test" + str(i))
+#     headers = ["Overall", "Author", "Journal", "Publish Date", "Author Order", "Publisher", "Percentage of Confidence", "Model"]
+#
+#     new_sheet.append(headers)
+#     for item in data:
+#         row_values = [item[key] for key in headers]
+#         new_sheet.append(row_values)
+#
+#
+# wb.save(filename= "output.xlsx")
+
+# make a final column of what the correct answer should be
+# need to create a full excel
+
