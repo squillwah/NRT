b4a54af189635f881810a81a615fd96499401a2f
squillwah <Swagapotato@gmail.com>
2026-06-29 15:39:46 -0400

    Internet search, but newline psychosis.

--- scripts/prompt_schemas.py
+++ scripts/prompt_schemas.py
@@ -37,7 +37,7 @@ def protoschema_verify(thing):   #, *, override_binary=None, override_continuous
                 "description": f"The percent probability that {thing} is real." # Is 'probability' the right framing?
             }
         },
-        "required": ["binary", "continuous"],
+        "required": ["binary", "continuous"],   # ! Todo: One thing to look at in the data is when they binary contradicts the continuous. Also, would changing the position of these in the schema effect responses? Could be a #experiment.
         "additionalProperties": False
     }
 def protoschema_classify(thing):
@@ -91,7 +91,7 @@ def gen_payload(reference, model, schema, context, *, search=True, datetime=True
             { "role": "system", "content": context },    # https://openrouter.ai/docs/api/reference/overview What about the 'name' one?
             { "role": "user", "content": reference }
         ],
-        "tools": [{ "type": tool } for tool, toggle in toolgles if toggle],     # https://openrouter.ai/docs/guides/features/server-tools/overview
+        #"tools": [{ "type": tool } for tool, toggle in toolgles if toggle],     # https://openrouter.ai/docs/guides/features/server-tools/overview
         "response_format": schema                                               # We can also define local tools, if that becomes useful. https://openrouter.ai/docs/guides/features/tool-calling
     }
 
@@ -99,13 +99,13 @@ def gen_payload(reference, model, schema, context, *, search=True, datetime=True
 # The date thing in the context is stupid. 
 # We can easily enable websearch and datetime fetching from OpenRouter, but that conflicts with structured responses.
 # To do both, we will need to run two passes per reference. One that does the websearching and thinking, and the other which thinks again and encodes in into a structured response.
-PROMPT_CONTEXT = """The date is 2026/06/28. You are a biomedical references verifier. Assess each component and examine the reference as a whole. Adhere to the strict structure response JSON schema.
-Definitions of categories:
-1. Verified: The article exists, and the title plus key metadata match the database record. - Minor formatting differences are acceptable.
-2. Metadata Error: The article exists, but one or more fields are wrong, incomplete, abbreviated, or formatted differently. - The article can still be confidently identified.
-3. Serious Metadata Error: The title or other major fields are wrong, but the DOI or PMID correctly points to a real article. - Not fabricated if the DOI/PMID identifies a real article.
-4. Plausible Fabricated: The claimed title cannot be matched to any real article after reasonable search. - Real authors, real journals, or plausible topics are not enough to prove the article exists.
-5. Needs Human Review: The evidence is mixed, weak, or ambiguous. - Use this when there are partial title matches, missing/conflicting DOI or PMID, or multiple possible matches."""
+PROMPT_CONTEXT = """The date is 2026/06/28. You are a biomedical references verifier. Assess each component and examine the reference as a whole. """ #Adhere to the strict structure response JSON schema.
+#Definitions of categories:
+#1. Verified: The article exists, and the title plus key metadata match the database record. - Minor formatting differences are acceptable.
+#2. Metadata Error: The article exists, but one or more fields are wrong, incomplete, abbreviated, or formatted differently. - The article can still be confidently identified.
+#3. Serious Metadata Error: The title or other major fields are wrong, but the DOI or PMID correctly points to a real article. - Not fabricated if the DOI/PMID identifies a real article.
+#4. Plausible Fabricated: The claimed title cannot be matched to any real article after reasonable search. - Real authors, real journals, or plausible topics are not enough to prove the article exists.
+#5. Needs Human Review: The evidence is mixed, weak, or ambiguous. - Use this when there are partial title matches, missing/conflicting DOI or PMID, or multiple possible matches."""
 
 # Dict of internal component tags (in schema/response) against descriptions of components (for prompt insertion, see protoschema templates).
 FOR_VERIFICATION = {
@@ -125,6 +125,8 @@ FOR_VERIFICATION = {
 }
 FOR_CLASSIFICATION = { "REFERENCE": "the reference" }
 
+# @todo: Eventually we may want gen_schema(<toggles for each component>) to avoid prompting llms about absent components.
+
 # For testing
 if __name__ == "__main__":
     import json
@@ -132,22 +134,29 @@ if __name__ == "__main__":
     import requests
 
 #    testmod = #"openai/gpt-oss-120b:free" #"openrouter/free"   # Auto choose one that supports structured responses + netsearch. For testing.      #"openai/gpt-oss-20b:free"
-    testmod = "openrouter/free"
-    testref = "Penner M, Zwaigenbaum L, Piroddi N, Park S, Minhas RS, Singal D. Advances in supporting development in autistic children and youth. BMJ. 2026;393:e086562. Published 2026 Jun 10. doi:10.1136/bmj-2025-086562"
 
     schema = gen_schema(verify=FOR_VERIFICATION, classify=FOR_CLASSIFICATION)
-    payload = gen_payload(testref, testmod, schema, PROMPT_CONTEXT, search=True, datetime=True)
-    print(json.dumps(payload, indent=2))
+    testref = "Penner M, Zwaigenbaum L, Piroddi N, Park S, Minhas RS, Singal D. Advances in supporting development in autistic children and youth. BMJ. 2026;393:e086562. Published 2026 Jun 10. doi:10.1136/bmj-2025-086562"
+
+    #testmod = "openai/gpt-oss-120b:free"
+    testmod = "openrouter/free:online"
+    payload = gen_payload(testref, testmod, schema, PROMPT_CONTEXT, search=False, datetime=False)
+
+    #print(json.dumps(payload, indent=2))
     print("\n\n\n")
 
     response = requests.post(url="https://openrouter.ai/api/v1/chat/completions",
                              headers={ "Authorization": f"Bearer {os.environ["key"]}", "Content-Type": "application/json" },
                              json=payload)
+    try: print(json.dumps(json.loads(response.request.body.decode("utf-8")), indent=2))
+    except: print(response.request)
+    print("\n\n")
+
     data = None
     try:
         data = response.json()
         print(json.dumps(data, indent=2))
     except: print(response.content)
     print("\n\n\n")
-    try: print(json.dumps(json.loads(data["choices"][0]["message"]["content"])))
-    except: print(response.content)
+    try: print(json.dumps(json.loads(data["choices"][0]["message"]["content"]), indent=2))
+    except: print(data["choices"][0]["message"]["content"])
