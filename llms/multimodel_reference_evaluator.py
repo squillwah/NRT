3dc7c5829b7e2f1d8c37e85f3d741c12feedd804
squillwah <Swagapotato@gmail.com>
2026-07-01 16:28:10 -0400

    Test again without internet, nemotron obeys schema, but it still has some confusion in its thinking.
    Seemingly like it doesn't know about the schema, worries about having to make one up, then finally outputs the actual schema.

--- llms/multimodel_reference_evaluator.py
+++ llms/multimodel_reference_evaluator.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         "Volk, R. J., Lewis, K. B., Smith, M., Carley, M., Barry, M. J., Bekker, H. L., H\u00e4rter, M., Hoffmann, T., McCaffery, K., Pignone, M., Steffensen, K. D., Sepucha, K., Thompson, R., Trevena, L., van der Weijden, T., Witteman, H. O., & Stacey, D. (2026). Updated International Patient Decision Aid Standards (IPDAS version 5.0): modified Delphi, evidence informed consensus process. BMJ (Clinical research ed.), 393, e088116. https://doi.org/10.1136/bmj-2025-088116"
     ]
     models = [
-            "nvidia/nemotron-3-super-120b-a12b:free:online",
-            "openai/gpt-oss-20b:free:online"
-    ]
+            "nvidia/nemotron-3-super-120b-a12b:free"]
+#            "openai/gpt-oss-20b:free"
+#    ]
 
     # Classify all components. Will trim this according to per reference specifics at some point (avoid confusion / forced hallucination)
     classify = {
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         for model in models:
             print(f"starting model {model}")
             r = parse_response(trytryagain(openrouter, {"model": model, "ref": ref, "schema": schema}))   # Call openrouter, retry three times if code != 200, then parse into {request, response_text, response_json, content} dict.
-            responses.append(r)
+            responses.append({k: r[k] for k in r if k != "request_json"})
             data[i][model] = r["content"]
             print("done")
             time.sleep(1)
