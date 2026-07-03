f18169889b863f7edf9388b291d1a425a4ae7fdc
squillwah <Swagapotato@gmail.com>
2026-07-02 20:27:46 -0400

    Use 'log' helper instead of print
    To easily redirect into a log file later.

--- llms/llmtools/orouterapi.py
+++ llms/llmtools/orouterapi.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import time
+from llmtools.help import log
 
 def make_payload_responses(model, ref, schema): pass
 def make_payload_completions(model, ref, schema):
@@ -30,7 +31,6 @@ def openrouter(model, ref, schema, *, api="completions", key=os.environ["THEKEY"
         case "responses":
             payload = make_payload_responses(model, ref, schema)
             endpoint = "/api/v1/responses"
-        case _: print(api, "typo stupid")
 
     return requests.post(url=f"https://openrouter.ai{endpoint}",
                          headers={"Authorization":f"Bearer {key}","Content-Type":"application/json" },
@@ -50,14 +50,15 @@ def parse_response(response, *, api="completions"):
                 case "completions":
                     try: processed["content"] = json.loads(processed["response_json"]["choices"][0]["message"]["content"])
                     except Exception as e:
-                        print(" !!! couldn't parse json from choices/message/content: ", e)
+                        log("Couldn't parse json from choices/message/content: ", e, t="e")
+                        input("Now would be the time to quit if something is wrong.")                               # @ Don't forget you put this here.
                         processed["content"] = processed["response_json"]["choices"][0]["message"]["content"]
                 case "responses": processed["content"] = "......"
-                case _: print(api, "typo stupid")
+                case _: log(api, "typo stupid", t="e")
         except Exception as e:
-            print(" !!! couldn't even get text of choices/message/content: ", e)
+            log("Couldn't even get text from [choices][0][message][content]: ", e, t="e")
     except Exception as e:
-        print(" !!! couldn't parse json from response: ", e)
+        log("Couldn't parse json from response: ", e, t="e")
 
     return processed
 
@@ -66,32 +67,32 @@ def trytryagain(request, kwargs, *, tries=3, wait=5):
     retry = False
     response = request(**kwargs)
     if response.status_code != 200:
-        print(f" !!! Openrouter request error, status_code: {response.status_code}")
+        log(f"Openrouter request error, status_code: {response.status_code}", t="e")
         retry = True
     # Catch upstream provider errors
     else:
         try:
-            rjson = response.json()# May be fragile, JSON parsing errors or false positives etc.
+            rjson = response.json() # May be fragile, JSON parsing errors or false positives etc.
             if "error" in rjson:
-                print(f" !!! Upstream error: {rjson["error"]}")
+                log(f"Upstream error: {rjson["error"]}", t="e")
                 retry = True
-            else: print("Request success!")
+            else: log("Request success!")
         except Exception as e:
-            print(f" !!! Something terrible has happened. \n\n{e}\n\n{response.text}\n\n")
+            log(f"Something terrible has happened. \n\n{e}\n\n{response.text}\n\n", t="e")
     if retry:
         if tries > 0:
-            print(f"      Trying again in {wait} seconds, {tries} tries left.")
+            log(f"Trying again in {wait} seconds, {tries} tries left.", t="s")
             time.sleep(wait)
             args["tries"] = args["tries"] - 1
             response = trytryagain(**args)
-        else: print(f"      No tries left, giving up. Message: \n\n{response.text}\n\n")
+        else: log(f"No tries left, giving up. Message: \n\n{response.text}\n\n", t="e")
     return response
 
 if __name__ == "__main__":
     m = "openai/gpt-oss-20b:free"
     r = "Volk, R. J., Lewis, K. B., Smith, M., Carley, M., Barry, M. J., Bekker, H. L., H\u00e4rter, M., Hoffmann, T., McCaffery, K., Pignone, M., Steffensen, K. D., Sepucha, K., Thompson, R., Trevena, L., van der Weijden, T., Witteman, H. O., & Stacey, D. (2026). Updated International Patient Decision Aid Standards (IPDAS version 5.0): modified Delphi, evidence informed consensus process. BMJ (Clinical research ed.), 393, e088116. https://doi.org/10.1136/bmj-2025-088116"
     s = "thescemer"
-    print(trytryagain(openrouter, {"model": m, "ref": r, "schema": s}).text)
+    log(trytryagain(openrouter, {"model": m, "ref": r, "schema": s}).text)
 
 
 
