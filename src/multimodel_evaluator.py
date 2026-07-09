a59a06e25ae167566101fb72a4e254277ffd2d28
squillwah <Swagapotato@gmail.com>
2026-07-09 15:48:04 -0400

    Rewriting most of evaluator, for scalability.

--- src/multimodel_evaluator.py
+++ src/multimodel_evaluator.py
@@ -1,8 +1,12 @@
 
+from tools.help import log 
 from tools.llms.schemas import ProtoSchemas
-#from tools.llms.orouterapi import openrouter, parse_response, trytryagain
-from tools.help import log, write_json
+from tools.llms.orouterapi import openrouter, parse_response, trytryagain
+from tools.references.refdata import ReferenceComponent
 from pathlib import Path
+from shutil import copy2
+from copy import deepcopy
+import tools.help as h
 import json
 import time
 import os
@@ -16,31 +20,88 @@ import os
 #      @consider: Should we enable reasoning?
 #                 Testing result difference with deepseek, internet on and off.
 
-from tools.help import log 
-import tools.help as h
 
-save_result = None
-save_request_dump = None
-def open_files():
-    d_root = h.mkdir("./mme", timestamp=True)
+# Set up output files
+save_dataset = None
+save_results = None
+save_response_dump = None
+def open_files(root):
+    d_root = h.mkdir(root, timestamp=True)
     d_results = h.mkdir(d_root / "results")
-    d_request_dumps = h.mkdir(d_root / "request_dumps")
+    d_response_dumps = h.mkdir(d_root / "dumps")
     h.open_log(d_root / "log")
 
-    global save_result, save_request_dump
-    save_result = lambda result, name: h.write_json(result, f"{d_results}/{name}.json")
-    save_request_dump = lambda dump, name: h.write_json(dump, f"{d_request_dumps}/{name}.json")
-
+    global save_dataset, save_results, save_response_dump
+    save_dataset = lambda filename: copy2(filename, d_root / "source.json")
+    save_results = lambda result, name: h.write_json(result, f"{d_results}/{name}.json")
+    save_response_dump = lambda dump, name: h.write_json(dump, f"{d_response_dumps}/{name}.json")
+# Clean up
 def close_files(): 
-    global save_result, save_request_dump
-    save_result, save_request_dump = None, None
+    global save_dataset, save_results, save_response_dump
+    save_dataset, save_results, save_response_dump = None, None, None
     h.close_log()
 
-open_files()
-log("this is a test")
-save_result({"test": "result"}, "1")
-save_request_dump({"test": "dump"}, "11")
-close_files()
+# Query openrouter for response, save dump to file and return model output
+def evaluate(dsentry, model, form=None):
+    schema = ProtoSchemas.make_schema(ProtoSchemas.make_schema_properties())        # Later on this can be tailored. A "prompt trimming" experiment (make it easier on the AI).
+    log("Requesting Openrouter", t="s")
+    response = trytryagain(openrouter, {"model": model, "ref": dsentry["format"]["elsevier"], "schema": schema})    # @TODO differentiate formats
+    log("Parsing response", t="s")
+    response = parse_response(response)     # @TODO record time ....
+    log(f"Saving response dump to '{dsentry["id"]}_{form}_{model.split("/")[-1]}.json'", t="s")
+    save_response_dump(response, f"{dsentry["id"]}_{form}_{model.split("/")[-1]}")
+    return response["result"]
+
+# Template for evaluated reference file structure
+def results_template(dsentry, models):
+    return {
+        "id": dsentry["id"],
+        "components": [c for c in ReferenceComponent]+["REFERENCE"],    # Just full list of components and the holistic REFERENCE. Mirrors compelte schema (untrimmed). # @TODO Should this be trimmed specific? Or a bool dict like in the dsentry? 
+        "models": models,                                               # Metadata to help with csv and grading.
+        "formats": {f: [] for f in FormatStyles},
+        "results": {m: {f: None for f in FormatStyles} for m in models}
+    }
+def add_result(model, style, result, results): 
+    results["results"][model][style] = result
+    if model not in results["formats"][style]:
+        results["formats"][style].append(model)
+
+MODELS = ("deepseek/deepseek-v4-flash", "nvidia/nemotron-3-super-120b-a12b:free")
+DATASET = "REFERENCE_DATASET2.json"
+TARGET_DIR = "./mme"
+
+# @PLACEHOLDER
+FormatStyles = ["elsevier"]
+
+if __name__ == "__main__":
+    open_files(TARGET_DIR)
+    log(f"Testing references in '{DATASET}' against:")
+    for m in MODELS: log(m, t="s") 
+    
+    log("Loading dataset, creating backup 'source.json' at root")
+    save_dataset(DATASET)
+    dataset = h.read_json(DATASET)
+    dataset = [entry for subset in dataset.values() for entry in subset]    # Placeholder until dataset generation is redone (flat structure)
+
+    log(f"Starting evaluation of {len(dataset)} references, with {len(MODELS)} models per reference")
+    for entry in dataset:
+        results = results_template(entry, MODELS)
+        
+        for model in MODELS:
+            style = random.choice(list(FormatStyles))                           # @TODO Pick formats intelligently? Is random a good metric, or should we weight by 'popularity' (whatever that may be).
+            
+            log(f"ENTRY {entry["id"]} +MODEL {model} +STYLE {style}", t="h")    # @TODO remap dataset generation as a dict of ID : data instead of array. Functionally the same but stronger convention.
+            result = evaluate(entry, model, form=style)                         # @TODO a FormatStyle enum to iterate through.
+            
+            log(f"Adding result: {model} ({style})")
+            if result: add_result(model, style, result, results)
+            else: log("BADBAD! Result is null and was not added, something terrible must have happened.", t="e")
+        
+        log(f"Saving full multimodel evaluation of ENTRY {entry["id"]} to '{entry["id"]}.json'")
+        save_results(results, f"{entry["id"]}")
+    
+    close_files()
+
 
 """
 
--- src/tools/help.py
+++ src/tools/help.py
@@ -14,28 +14,42 @@ def open_log(filename, keep_print=True):
     writer = lambda msg: _LOGFILE.write(msg+"\n")
     if keep_print: _LOGOUT = lambda msg: [f(msg) for f in (writer, print)]
     else: _LOGOUT = writer
+    
+    _LOGOUT(f"\n {time.strftime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))}\n")
 
 def close_log(): 
-    if _LOGFILE: _LOGFILE.close()
+    global _LOGFILE, _LOGOUT
+    if _LOGFILE: 
+        log("Closing log file.")
+        _LOGOUT = print
+        _LOGFILE.close()
+        _LOGFILE = None
 
 def log(*messages, delim=" ", t="log"):
     # Can redirect these to a file later.
     messages = delim.join([str(m) for m in messages])
     if t in ("log", "l"):   t = ">> "
-    elif t in ("sup", "s"): t = "|| "
+    elif t in ("sup", "s"): t = " *  "
     elif t in ("err", "e"): t = "!! "
+    elif t in ("hed", "h"): 
+        t = "== "
+        messages = messages + " =="
     
-    _LOGOUT(f"{t}{messages}")
+    _LOGOUT(f"[{time.strftime("%H:%M:%S", time.localtime())}] {t}{messages}")
 
 def mkdir(name, timestamp=False):
-    d = Path(f"{name}_{int(time.time())}" if timestamp else name)
+    #d = Path(f"{name}_{int(time.time())}" if timestamp else name)
+    d, i = Path(name), 0
+    while d.exists(): 
+        i = i + 1
+        d = Path(f"{name}{i}")
     d.mkdir()
     return d
 
 def write_json(data, filename, access="w"):
-    with open(filename, access) as file:
+    with open(filename, access, buffering=1) as file:
         try: json.dump(data, file, indent=2)
-        except:
+        except Exception as e:
             log(f"JSON conversion failed\n\n{e}\n, writing plaintext...", t="e")
             file.write(data)
 
