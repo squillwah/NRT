7637ab2c8b1a34d3f75c70f30da704931eab121f
squillwah <Swagapotato@gmail.com>
2026-07-09 17:42:58 -0400

    Using FormatStyle, iterating by dataset ID keys, saving to an 'all' file at the end.

--- src/multimodel_evaluator.py
+++ src/multimodel_evaluator.py
@@ -3,13 +3,13 @@ from tools.help import log
 from tools.llms.schemas import ProtoSchemas
 from tools.llms.orouterapi import openrouter, parse_response, trytryagain
 from tools.references.refdata import ReferenceComponent
-from pathlib import Path
+from tools.references.formats import FormatStyle
 from shutil import copy2
-from copy import deepcopy
 import tools.help as h
 import json
-import time
-import os
+import random
+#import time
+#import os
 
 # @todo add keys array to JSON output.
 
@@ -30,6 +30,7 @@ def open_files(root):
     d_results = h.mkdir(d_root / "results")
     d_response_dumps = h.mkdir(d_root / "dumps")
     h.open_log(d_root / "log")
+    log("Directory:", str(d_root), t="h")
 
     global save_dataset, save_results, save_response_dump
     save_dataset = lambda filename: copy2(filename, d_root / "source.json")
@@ -48,8 +49,8 @@ def evaluate(dsentry, model, form=None):
     response = trytryagain(openrouter, {"model": model, "ref": dsentry["format"]["elsevier"], "schema": schema})    # @TODO differentiate formats
     log("Parsing response", t="s")
     response = parse_response(response)     # @TODO record time ....
-    log(f"Saving response dump to '{dsentry["id"]}_{form}_{model.split("/")[-1]}.json'", t="s")
-    save_response_dump(response, f"{dsentry["id"]}_{form}_{model.split("/")[-1]}")
+    log(f"Saving response dump to '{dsentry["id"]}_{model.split("/")[-1]}_{form}.json'", t="s")
+    save_response_dump(response, f"{dsentry["id"]}_{model.split("/")[-1]}_{form}")
     return response["result"]
 
 # Template for evaluated reference file structure
@@ -58,20 +59,17 @@ def results_template(dsentry, models):
         "id": dsentry["id"],
         "components": [c for c in ReferenceComponent]+["REFERENCE"],    # Just full list of components and the holistic REFERENCE. Mirrors compelte schema (untrimmed). # @TODO Should this be trimmed specific? Or a bool dict like in the dsentry? 
         "models": models,                                               # Metadata to help with csv and grading.
-        "formats": {f: [] for f in FormatStyles},
-        "results": {m: {f: None for f in FormatStyles} for m in models}
+        "formats": {},
+        "results": {m: {f: None for f in FormatStyle} for m in models}  # @TODO add time and token metrics (probably return as part of result in evaluate)
     }
 def add_result(model, style, result, results): 
     results["results"][model][style] = result
-    if model not in results["formats"][style]:
-        results["formats"][style].append(model)
+    if style not in results["formats"]: results["formats"][style] = [model] 
+    elif model not in results["formats"][style]: results["formats"][style].append(model)
 
 MODELS = ("deepseek/deepseek-v4-flash", "nvidia/nemotron-3-super-120b-a12b:free")
-DATASET = "REFERENCE_DATASET2.json"
+DATASET = "THEREFERENCES_IDKEYS.json"
 TARGET_DIR = "./mme"
-
-# @PLACEHOLDER
-FormatStyles = ["elsevier"]
 
 if __name__ == "__main__":
     open_files(TARGET_DIR)
@@ -81,27 +79,43 @@ if __name__ == "__main__":
     log("Loading dataset, creating backup 'source.json' at root")
     save_dataset(DATASET)
     dataset = h.read_json(DATASET)
-    dataset = [entry for subset in dataset.values() for entry in subset]    # Placeholder until dataset generation is redone (flat structure)
 
-    log(f"Starting evaluation of {len(dataset)} references, with {len(MODELS)} models per reference")
-    for entry in dataset:
+    # Trim to 2 for testing
+    dataset = {ID: entry for ID, entry in list(dataset.items())[:2]}
+
+    log(f"Beginning evaluation of {len(dataset)} references, with {len(MODELS)} models per reference")
+    
+    results_all = {ID: None for ID in dataset}
+    for ID, entry in dataset.items():
+        log(f"Generating result template for {entry["id"]}")
         results = results_template(entry, MODELS)
         
+        log(f"Iterating models...")
         for model in MODELS:
-            style = random.choice(list(FormatStyles))                           # @TODO Pick formats intelligently? Is random a good metric, or should we weight by 'popularity' (whatever that may be).
+            style = random.choice(list(FormatStyle))        # @TODO Pick formats intelligently? Is random a good metric, or should we weight by 'popularity' (whatever that may be).
             
-            log(f"ENTRY {entry["id"]} +MODEL {model} +STYLE {style}", t="h")    # @TODO remap dataset generation as a dict of ID : data instead of array. Functionally the same but stronger convention.
-            result = evaluate(entry, model, form=style)                         # @TODO a FormatStyle enum to iterate through.
+            log(f"{ID} {model} {style}", t="h")
+            result = evaluate(entry, model, form=style)
             
-            log(f"Adding result: {model} ({style})")
+            log(f"Adding result: {model} ({style})", t="s")
             if result: add_result(model, style, result, results)
             else: log("BADBAD! Result is null and was not added, something terrible must have happened.", t="e")
         
-        log(f"Saving full multimodel evaluation of ENTRY {entry["id"]} to '{entry["id"]}.json'")
-        save_results(results, f"{entry["id"]}")
+        log(f"Saving full multimodel evaluation of ENTRY {ID} to '{ID}.json' and appending to 'all.json'")
+        save_results(results, f"{ID}")
+        results_all[ID] = results
+
+    log(f"Saving all results to 'all.json'")
+    save_results(results_all, "all")
+
     
     close_files()
 
+
+# Some experiments to do regarding schema trimming
+# See performance without schema trimming, but with disregarding chatbot answers
+# See performance with schema trimming (so no answers in the first place)
+# And of course the full load. How does the existence of the question effect their performance?
 
 """
 
