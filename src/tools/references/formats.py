e31957c543c70ad82af3f660c96136561c574999
squillwah <Swagapotato@gmail.com>
2026-07-13 05:16:00 -0400

    Progressing toward ref builder

--- src/tools/references/formats.py
+++ src/tools/references/formats.py
@@ -8,7 +8,8 @@ class FormatStyle(StrEnum):
     OXFORD    = "oxford",
     SPRINGER  = "springer",
     CSE       = "cse",
-    HARVARD   = "harvard"
+    HARVARD   = "harvard",
+    VANCOUVER = "vancouver"
 
 # Standards
 # AMA
@@ -22,273 +23,101 @@ class FormatStyle(StrEnum):
 # Elsevier
 # Springer
 
+#Vancouver / NLM
+#PubMed
+#AMA
+#ICMJE
+#Nature
+#Cell Press
+#Science (AAAS)
+#PNAS
+#Elsevier Vancouver
+#Springer Nature
+#Wiley
+#eLife
+#Oxford University Press
+#ACS (biochemistry journals)
+# IEEE
+
+from refdata import ReferenceComponent as RC
+
+# PMC Citing Medicine Guide
+# https://www.ncbi.nlm.nih.gov/books/NBK7256/
+
 class Formats:
-    _AUTHOR_PRESETS = {
-        "vancouver": {
-            "ea_threshold": 6,
-            "delim_authors": ", ",
-            "delim_initials": "",
-            "delim_lastfirst": " ",
-            "initialize": True,
-            "initialize_periods": False
+    _STYLE_CONFIGS = {
+        # How the author list is styled,.
+        FormatStyle.VANCOUVER: {
+            RC.AUTHORS: {
+                "etal": ", et al",              # Include comma at start if desired.    ", et al" or " et al"
+                "etal_threshold": 6,
+                "delim_authors": ", ",
+                "delim_initials": "",
+                "delim_lastfirst": " ",
+                "initialize": True,
+                "initialize_periods": False,
+                "penultimate_and": "",          # Include comma at start for oxford comma. ", and " or " & " or ", & "  etc.
+            },
+            RC.JOURNAL_NAME: "short",    # Or 'full'
+            "ORDER": [RC.AUTHORS, RC.TITLE]
         }
     }
+        
+    #_COMPONENT_PROCESSING = {
+    #    RC.AUTHORS:             Formats._authorize
+    #    RC.TITLE:               lambda title: title
+    #    RC.JOURNAL_NAME:        lambda 
+    #    RC.JOURNAL_VOLUME:  
+    #    RC.JOURNAL_ISSUE:   
+    #    RC.JOURNAL_PAGE:    
+    #    RC.ELOCATOR:        
+    #    RC.PUBLICATION_DATE:
+    #    RC.DOI:             
+    #    RC.PMID:            
+    #    RC.PMCID:           
+    #}
 
     @staticmethod
-    def authorize(authors, *, ea_threshold=None, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods):       # Some lasts will already be initialized, but may lack periods.
+    def _authorize(authors, *, etal, etal_threshold, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods, penultimate_and):       # Some lasts will already be initialized, but may lack periods.
         names = [(a["l"], a["f"]) for a in authors]
-        ea = ea_threshold is not None and len(names) > ea_threshold
-        if ea: names = names[:ea_threshold]
+        ea = etal_threshold is not None and len(names) > etal_threshold
+        if ea: names = names[:etal_threshold]
         for i, (last, firsts) in enumerate(names):
             if initialize and firsts:    # Not all have first names to be initialized.
                 firsts = [i[0].upper()+("." if initialize_periods else "") for i in firsts.split()] # Will we ever get empty from split?
                 firsts = delim_initials.join(firsts)
             names[i] = delim_lastfirst.join((last, firsts)) if firsts else last     # Condition avoids trailing ' ' when no first names.
-        if ea: names.append("et al")
-        names = delim_authors.join(names)
-        print(names)
-        #print(locals())
-        #formatted = ([None] * ea_threshold + ["et al"]) if ea_threshold is not None and len(names) > ea_threshold else ([None] * len(names))
-        #print(formatted)
+        if penultimate_and and not ea:
+            names = delim_authors.join(names[:-1]) + penultimate_and + names[-1]
+        else:
+            names = delim_authors.join(names) + etal*ea
+        return names
 
-ass = [
-      {
-        "l": "Akbari",
-        "f": "R"
-      },
-      {
-        "l": "de Araujo Azevedo",
-        "f": "L O"
-      },
-      {
-        "l": "Baker",
-        "f": "C J"
-      },
-      {
-        "l": "Bertsche",
-        "f": "W"
-      },
-      {
-        "l": "Bhatt",
-        "f": "N M"
-      },
-      {
-        "l": "Bonomi",
-        "f": "G"
-      },
-      {
-        "l": "Capra",
-        "f": "A"
-      },
-      {
-        "l": "Carli",
-        "f": "I"
-      },
-      {
-        "l": "L Cesar",
-        "f": "C"
-      },
-      {
-        "l": "Charlton",
-        "f": "M"
-      },
-      {
-        "l": "Cridland Mathad",
-        "f": "A"
-      },
-      {
-        "l": "Del Vincio",
-        "f": "A"
-      },
-      {
-        "l": "Duque Quiceno",
-        "f": "D"
-      },
-      {
-        "l": "Eriksson",
-        "f": "S"
-      },
-      {
-        "l": "Evans",
-        "f": "A"
-      },
-      {
-        "l": "Fajans",
-        "f": "J"
-      },
-      {
-        "l": "Friesen",
-        "f": "T"
-      },
-      {
-        "l": "Fujiwara",
-        "f": "M C"
-      },
-      {
-        "l": "Golino",
-        "f": "L M"
-      },
-      {
-        "l": "Gomes Gon\u00e7alves",
-        "f": "M B"
-      },
-      {
-        "l": "Hangst",
-        "f": "J S"
-      },
-      {
-        "l": "Hayden",
-        "f": "M E"
-      },
-      {
-        "l": "Heidari",
-        "f": "P"
-      },
-      {
-        "l": "Hodgkinson",
-        "f": "D"
-      },
-      {
-        "l": "Isaac",
-        "f": "C A"
-      },
-      {
-        "l": "Jones",
-        "f": "S A"
-      },
-      {
-        "l": "Jonsell",
-        "f": "S"
-      },
-      {
-        "l": "Madsen",
-        "f": "N"
-      },
-      {
-        "l": "Marshall",
-        "f": "V R"
-      },
-      {
-        "l": "McKenna",
-        "f": "J T K"
-      },
-      {
-        "l": "Momose",
-        "f": "T"
-      },
-      {
-        "l": "Nauta",
-        "f": "J"
-      },
-      {
-        "l": "Oliveira",
-        "f": "A N"
-      },
-      {
-        "l": "Powell",
-        "f": "A"
-      },
-      {
-        "l": "Rasmussen",
-        "f": "C \u00d8"
-      },
-      {
-        "l": "Robertson-Brown",
-        "f": "T"
-      },
-      {
-        "l": "Robicheaux",
-        "f": "F"
-      },
-      {
-        "l": "Sacramento",
-        "f": "R L"
-      },
-      {
-        "l": "Sarid",
-        "f": "E"
-      },
-      {
-        "l": "Schoonwater",
-        "f": "J"
-      },
-      {
-        "l": "Silveira",
-        "f": "D M"
-      },
-      {
-        "l": "Singh",
-        "f": "J"
-      },
-      {
-        "l": "Smith",
-        "f": "G"
-      },
-      {
-        "l": "So",
-        "f": "C"
-      },
-      {
-        "l": "Stracka",
-        "f": "S"
-      },
-      {
-        "l": "Suh",
-        "f": "J"
-      },
-      {
-        "l": "Swadling",
-        "f": "A G"
-      },
-      {
-        "l": "Tharp",
-        "f": "T D"
-      },
-      {
-        "l": "Thompson",
-        "f": "K A"
-      },
-      {
-        "l": "Thompson",
-        "f": "R I"
-      },
-      {
-        "l": "Thorpe-Woods",
-        "f": "E"
-      },
-      {
-        "l": "Uribe Jimenez",
-        "f": "A J"
-      },
-      {
-        "l": "Urioni",
-        "f": "M"
-      },
-      {
-        "l": "van de Werf",
-        "f": "D P"
-      },
-      {
-        "l": "Wilson",
-        "f": "S G"
-      },
-      {
-        "l": "ALPHA Collaboration",
-        "f": ""
-      },
-      {
-        "l": "Woosaree",
-        "f": "P"
-      },
-      {
-        "l": "Wurtele",
-        "f": "J S"
-      }
-    ]
+    @classmethod
+    def build(cls, refdata, style):    # components: list[ReferenceComponent]
+        # Combine given components in sequential order
+        _COMPONENT_PROCESSING = {
+            RC.AUTHORS:             lambda rd, s: cls._authorize(refdata["authors"], **cls._STYLE_CONFIGS[s][RC.AUTHORS]),
+            RC.TITLE:               lambda rd, s: rd["title"], # Special processing?
+            RC.JOURNAL_NAME:        lambda rd, s: rd["journal"]["name"][cls._STYLE_CONFIGS[s][RC.JOURNAL_NAME]],
+            RC.JOURNAL_VOLUME:      lambda rd, s: rd["journal"]["volume"],  # Special styling ? (Paranthesis, semicolons and stuff?)
+            RC.JOURNAL_ISSUE:       lambda rd, s: rd["journal"]["issue"],
+            RC.JOURNAL_PAGE:        lambda rd, s: str(rd["journal"]["page"]["start"]) + "-" + str(rd["journal"]["page"]["end"]),
+            RC.ELOCATOR:            lambda rd, s: rd["journal"]["elocator"],
+            RC.PUBLICATION_DATE:    lambda rd, s: rd["pub"], # or rd["epub"] ? Hopefully deteremined by style. As well as y/m/d.
+            RC.DOI:                 lambda rd, s: rd["doi"]["prefix"] + "/" + rd["doi"]["suffix"], # Style should determine https:// inclusion etc.
+            RC.PMID:                lambda rd, s: rd["pmid"],
+            RC.PMCID:               lambda rd, s: rd["pmcid"]
+        }
 
+        for c in _COMPONENT_PROCESSING:
+            print(_COMPONENT_PROCESSING[c](refdata, style))
 
-
-Formats.authorize(ass, **Formats._AUTHOR_PRESETS["vancouver"])
+import h
+if __name__ == "__main__":
+    refs = h.read_json("refset.json", intkeys=True)
+    Formats.build(refs[88]["data"], FormatStyle.VANCOUVER)
+    #Formats._authorize(ass, **Formats._STYLE_CONFIGS[FormatStyle.VANCOUVER]["authors"])
 
 """
 # all the individual functions for each of the aforementioned bs citations are listed below.
