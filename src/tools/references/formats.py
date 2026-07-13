6e8fbdeba391a6d0cd856ebd577689d351374ffc
squillwah <Swagapotato@gmail.com>
2026-07-13 19:06:15 -0400

    more of it

--- src/tools/references/formats.py
+++ src/tools/references/formats.py
@@ -1,6 +1,7 @@
 import json
 import random
 from enum import StrEnum
+from refdata import ReferenceComponent
 
 class FormatStyle(StrEnum):
     ELSEVIER  = "elsevier",
@@ -10,6 +11,19 @@ class FormatStyle(StrEnum):
     CSE       = "cse",
     HARVARD   = "harvard",
     VANCOUVER = "vancouver"
+
+class CitationElement(StrEnum):
+    AUTHORS          = "authors"
+    TITLE            = "title"
+    JOURNAL_NAME     = "journal_name"
+    PUBLICATION_DATE = "publication_date"
+    JOURNAL_META     = "journal_meta"   # Comprises volume, issue, pages/elocator
+    IDENTIFIERS      = "identifiers"    # DOI or other database IDs
+    
+MONTHS = {1:  "Jan", 2:  "Feb", 3:  "Mar",
+          4:  "Apr", 5:  "May", 6:  "Jun",
+          7:  "Jul", 8:  "Aug", 9:  "Sep",
+          10: "Oct", 11: "Nov", 12: "Dec"}
 
 # Standards
 # AMA
@@ -39,16 +53,94 @@ class FormatStyle(StrEnum):
 #ACS (biochemistry journals)
 # IEEE
 
-from refdata import ReferenceComponent as RC
-
 # PMC Citing Medicine Guide
 # https://www.ncbi.nlm.nih.gov/books/NBK7256/
 
+FS = FormatStyle
+CE = CitationElement
+
+# Functions which build each element of a citation. Configurable.
+class ElementBuilder: 
+    def _authors(authors, *, etal, etal_threshold, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods, penultimate_and):       # Some lasts will already be initialized, but may lack periods.
+        names = [(a["l"], a["f"]) for a in authors]
+        ea = etal_threshold is not None and len(names) > etal_threshold
+        if ea: names = names[:etal_threshold]
+        for i, (last, firsts) in enumerate(names):
+            if initialize and firsts:    # Not all have first names to be initialized.
+                firsts = [i[0].upper()+("." if initialize_periods else "") for i in firsts.split()] # Will we ever get empty from split?    #@TODO Sometimes author lastnames are hyphened, so split at hyphen?
+                firsts = delim_initials.join(firsts)
+            names[i] = delim_lastfirst.join((last, firsts)) if firsts else last     # Condition avoids trailing ' ' when no first names.
+        if penultimate_and and not ea:
+            names = delim_authors.join(names[:-1]) + penultimate_and + names[-1]
+        else:
+            names = delim_authors.join(names) + etal*ea
+        return names
+
+    def _journal_name(names, abbreviate):
+        return names["short"] if abbreviate else names["full"]
+    
+    def _journal_meta(vol, iss, page, journal, styler):
+        return styler(vol=journal["volume"], iss=journal["issue"], page=journal["page"], elocator=journal["elocator"])  # "stylers" are fstring lambdas
+    
+    def _publication_date(pub, epub, styler):   
+        return styler(pub=pub, epub=epub)
+    
+    def _identifiers(doi, pmid, pmcid, styler): 
+        return styler(doi=doi, pmid=pmid, pmcid=pmcid)
+   
+    # Same interface wrappers around builder methods, associated with element type constants.
+    _COMPONENT_MAP = {
+        CE.TITLE:            lambda ref, conf: refdata["title"],  #_title(refdata["title"], **settings),
+        CE.AUTHORS:          lambda ref, conf: ElementBuilder._authors(refdata["authors"], **conf),
+        CE.JOURNAL_NAME:     lambda ref, conf: ElementBuilder._journal_name(refdata["journal"]["name"], **conf),
+        CE.JOURNAL_META:     lambda ref, conf: ElementBuilder._journal_meta(vol=refdata["journal"]["volume"], iss=refdata["journal"]["issue"], page=refdata["journal"]["page"], elocator=refdata["journal"]["elocator"], **conf),
+        CE.PUBLICATION_DATE: lambda ref, conf: ElementBuilder._publication_date(pub=refdata["pub"], epub=refdata["epub"], **conf),   # Might be cleaner to nest pub/epub under 'date' instead of flat. Same with identifiers? who cares
+        CE.IDENTIFIERS:      lambda ref, conf: ElementBuilder._identifiers(doi=refdata["doi"], pmid=refdata["pmid"], pmcid=refdata["pmcid"], **conf)
+    }
+   
+    # Returns reference to lambdas which call builder functions.
+    @classmethod
+    def build(cls, element): 
+        return cls._COMPONENT_MAP[element]
+
 class Formats:
-    _STYLE_CONFIGS = {
-        # How the author list is styled,.
-        FormatStyle.VANCOUVER: {
-            RC.AUTHORS: {
+    class ComponentStylers:
+        def meta_vancouver(vol, iss, page, elocator):
+            styled = ""
+            if vol:
+                styled = str(vol)
+                if iss: 
+                    styled += f"({iss})"
+            if page["start"] or page["end"]:
+                styled += ":"
+                if page["start"]:
+                    styled += str(page["start"])
+                    if page["end"]: styled += "-" + str(page["end"])
+                else: styled += str(page["end"])
+            if elocator:
+                styled += f":{elocator}"
+
+        def pub_vancouver(pub, epub):
+            styled = ""
+            date = pub if pub["y"] else epub
+            if date["y"]:
+                styled = str(date["y"])
+                if date["m"]:
+                    styled += " " + MONTHS[date["m"]]
+                    if date["d"]:
+                        styled += " " + str(date["d"])
+            return styled
+
+    # Ordering/combining of citation elements
+    _LAYOUT = {
+        FS.VANCOUVER: lambda elements: f"{elements[CE.AUTHORS]}. {elements[CE.TITLE]}. {elements[CE.JOURNAL_NAME]}. {elements[CE.PUBLICATION_DATE]};{elements[CE.JOURNAL_META]}. {elements[CE.IDENTIFIERS]}"
+    }
+
+    # Styling configuration of citation elements
+    _STYLE_CONFIG = {
+        # https://auckland.libguides.com/vancouver/journal-article
+        FS.VANCOUVER: {
+            CE.AUTHORS: {
                 "etal": ", et al",              # Include comma at start if desired.    ", et al" or " et al"
                 "etal_threshold": 6,
                 "delim_authors": ", ",
@@ -58,10 +150,29 @@ class Formats:
                 "initialize_periods": False,
                 "penultimate_and": "",          # Include comma at start for oxford comma. ", and " or " & " or ", & "  etc.
             },
-            RC.JOURNAL_NAME: "short",    # Or 'full'
-            "ORDER": [RC.AUTHORS, RC.TITLE]
+            CE.TITLE: {
+                None
+            },
+            CE.JOURNAL_NAME: {
+                "abbreviate": True
+            },
+            CE.JOURNAL_META: {
+                "styler": ComponentStylers.meta_vancouver
+            },
+            CE.PUBLICATION_DATE: {
+                "styler": lambda pub, epub: Formats._Stylers.monthy_publication(pub) if pub["y"] else epub   # Would be better to have the actual element be null and not dict of "". So it goes.
+                #lambda pub, epub: f"{pub["y"]}{f" {MONTHS[pub["m"]]{f" {pub["d"]}" if pub["d"] else ""}}" if pub["m"] else ""}"
+            },
+            CE.IDENTIFIERS: {
+                "styler": lambda doi, pmid, pmcid: f"Available from: doi:{doi["suffix"]}/{doi["prefix"]}"   # Maybe vancouver just shouldn't do DOI? Or it should do URLs instead? Variation needed...
+            }
         }
     }
+   
+    @classmethod
+    def build(cls, refdata, style):
+        elements = {ce: cls._ElementBuilder.build(ce)(refdata, cls._STYLE_CONFIG[style][ce]) for ce in CE}    # @TODO factor the omissions into this.
+        print(cls._LAYOUT[style](elements))
         
     #_COMPONENT_PROCESSING = {
     #    RC.AUTHORS:             Formats._authorize
@@ -77,45 +188,32 @@ class Formats:
     #    RC.PMCID:           
     #}
 
-    @staticmethod
-    def _authorize(authors, *, etal, etal_threshold, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods, penultimate_and):       # Some lasts will already be initialized, but may lack periods.
-        names = [(a["l"], a["f"]) for a in authors]
-        ea = etal_threshold is not None and len(names) > etal_threshold
-        if ea: names = names[:etal_threshold]
-        for i, (last, firsts) in enumerate(names):
-            if initialize and firsts:    # Not all have first names to be initialized.
-                firsts = [i[0].upper()+("." if initialize_periods else "") for i in firsts.split()] # Will we ever get empty from split?
-                firsts = delim_initials.join(firsts)
-            names[i] = delim_lastfirst.join((last, firsts)) if firsts else last     # Condition avoids trailing ' ' when no first names.
-        if penultimate_and and not ea:
-            names = delim_authors.join(names[:-1]) + penultimate_and + names[-1]
-        else:
-            names = delim_authors.join(names) + etal*ea
-        return names
 
-    @classmethod
-    def build(cls, refdata, style):    # components: list[ReferenceComponent]
-        # Combine given components in sequential order
-        _COMPONENT_PROCESSING = {
-            RC.AUTHORS:             lambda rd, s: cls._authorize(refdata["authors"], **cls._STYLE_CONFIGS[s][RC.AUTHORS]),
-            RC.TITLE:               lambda rd, s: rd["title"], # Special processing?
-            RC.JOURNAL_NAME:        lambda rd, s: rd["journal"]["name"][cls._STYLE_CONFIGS[s][RC.JOURNAL_NAME]],
-            RC.JOURNAL_VOLUME:      lambda rd, s: rd["journal"]["volume"],  # Special styling ? (Paranthesis, semicolons and stuff?)
-            RC.JOURNAL_ISSUE:       lambda rd, s: rd["journal"]["issue"],
-            RC.JOURNAL_PAGE:        lambda rd, s: str(rd["journal"]["page"]["start"]) + "-" + str(rd["journal"]["page"]["end"]),
-            RC.ELOCATOR:            lambda rd, s: rd["journal"]["elocator"],
-            RC.PUBLICATION_DATE:    lambda rd, s: rd["pub"], # or rd["epub"] ? Hopefully deteremined by style. As well as y/m/d.
-            RC.DOI:                 lambda rd, s: rd["doi"]["prefix"] + "/" + rd["doi"]["suffix"], # Style should determine https:// inclusion etc.
-            RC.PMID:                lambda rd, s: rd["pmid"],
-            RC.PMCID:               lambda rd, s: rd["pmcid"]
-        }
-
-        for c in _COMPONENT_PROCESSING:
-            print(_COMPONENT_PROCESSING[c](refdata, style))
+#    @classmethod
+#    def build(cls, refdata, style):    # components: list[ReferenceComponent]
+#        # Combine given components in sequential order
+#        _COMPONENT_PROCESSING = {
+#            RC.AUTHORS:             lambda rd, s: cls._authorize(refdata["authors"], **cls._STYLE_CONFIGS[s][RC.AUTHORS]),
+#            RC.TITLE:               lambda rd, s: rd["title"], # Special processing?
+#            RC.JOURNAL_NAME:        lambda rd, s: rd["journal"]["name"][cls._STYLE_CONFIGS[s][RC.JOURNAL_NAME]],
+#            RC.JOURNAL_VOLUME:      lambda rd, s: rd["journal"]["volume"],  # Special styling ? (Paranthesis, semicolons and stuff?)
+#            RC.JOURNAL_ISSUE:       lambda rd, s: rd["journal"]["issue"],
+#            RC.JOURNAL_PAGE:        lambda rd, s: str(rd["journal"]["page"]["start"]) + "-" + str(rd["journal"]["page"]["end"]),
+#            RC.ELOCATOR:            lambda rd, s: rd["journal"]["elocator"],
+#            RC.PUBLICATION_DATE:    lambda rd, s: rd["pub"], # or rd["epub"] ? Hopefully deteremined by style. As well as y/m/d.
+#            RC.DOI:                 lambda rd, s: rd["doi"]["prefix"] + "/" + rd["doi"]["suffix"], # Style should determine https:// inclusion etc.
+#            RC.PMID:                lambda rd, s: rd["pmid"],
+#            RC.PMCID:               lambda rd, s: rd["pmcid"]
+#        }
+#
+#        for c in _COMPONENT_PROCESSING:
+#            print(_COMPONENT_PROCESSING[c](refdata, style))
 
 import h
 if __name__ == "__main__":
+    pass
     refs = h.read_json("refset.json", intkeys=True)
+#    Formats.build(refs[88]["data"], FormatStyle.VANCOUVER)
     Formats.build(refs[88]["data"], FormatStyle.VANCOUVER)
     #Formats._authorize(ass, **Formats._STYLE_CONFIGS[FormatStyle.VANCOUVER]["authors"])
 
