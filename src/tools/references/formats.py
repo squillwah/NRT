55f88341cfabd98673acd67bf65c02f8d8d25fc0
squillwah <Swagapotato@gmail.com>
2026-07-14 00:26:36 -0400

    APA

--- src/tools/references/formats.py
+++ src/tools/references/formats.py
@@ -4,13 +4,17 @@ from enum import StrEnum
 from refdata import ReferenceComponent
 
 class FormatStyle(StrEnum):
+    VANCOUVER = "vancouver"
+    APA = "apa"
+
+
+
     ELSEVIER  = "elsevier",
     NATURE    = "nature",
     OXFORD    = "oxford",
     SPRINGER  = "springer",
     CSE       = "cse",
     HARVARD   = "harvard",
-    VANCOUVER = "vancouver"
 
 class CitationElement(StrEnum):
     AUTHORS          = "authors"
@@ -20,10 +24,6 @@ class CitationElement(StrEnum):
     JOURNAL_META     = "journal_meta"   # Comprises volume, issue, pages/elocator
     IDENTIFIERS      = "identifiers"    # DOI or other database IDs
     
-MONTHS = {1:  "Jan", 2:  "Feb", 3:  "Mar",
-          4:  "Apr", 5:  "May", 6:  "Jun",
-          7:  "Jul", 8:  "Aug", 9:  "Sep",
-          10: "Oct", 11: "Nov", 12: "Dec"}
 
 # Standards
 # AMA
@@ -56,46 +56,50 @@ MONTHS = {1:  "Jan", 2:  "Feb", 3:  "Mar",
 # PMC Citing Medicine Guide
 # https://www.ncbi.nlm.nih.gov/books/NBK7256/
 
-FS = FormatStyle
 CE = CitationElement
 
 # Functions which build each element of a citation. Configurable.
 class ElementBuilder: 
-    def _authors(authors, *, etal, etal_threshold, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods, penultimate_and):       # Some lasts will already be initialized, but may lack periods.
+    def authors(authors, *, etal, etal_apa=False, etal_threshold, delim_authors, delim_initials, delim_lastfirst, initialize, initialize_periods, penultimate_and):       # Some lasts will already be initialized, but may lack periods.
         names = [(a["l"], a["f"]) for a in authors]
         ea = etal_threshold is not None and len(names) > etal_threshold
-        if ea: names = names[:etal_threshold]
+        if ea: names = names[:etal_threshold] + ([names[-1]] if etal_apa else []) # APA etal mode appends the absolute final author.
+        # Transform 'names' into list of author name strings as configured.
         for i, (last, firsts) in enumerate(names):
             if initialize and firsts:    # Not all have first names to be initialized.
                 firsts = [i[0].upper()+("." if initialize_periods else "") for i in firsts.split()] # Will we ever get empty from split?    #@TODO Sometimes author lastnames are hyphened, so split at hyphen?
                 firsts = delim_initials.join(firsts)
             names[i] = delim_lastfirst.join((last, firsts)) if firsts else last     # Condition avoids trailing ' ' when no first names.
+        # Etalify 
         if penultimate_and and not ea:
             names = delim_authors.join(names[:-1]) + penultimate_and + names[-1]
+        elif ea:
+            if etal_apa: names = delim_authors.join(names[:-1]) + etal + names[-1]  # Smith, J., ... Jane, M.
+            else: names = delim_authors.join(names) + etal
         else:
-            names = delim_authors.join(names) + etal*ea
+            names = delim_authors.join(names)
         return names
 
-    def _journal_name(names, abbreviate):
+    def journal_name(names, abbreviate):
         return names["short"] if abbreviate else names["full"]
-    
-    def _journal_meta(vol, iss, page, journal, styler):
-        return styler(vol=journal["volume"], iss=journal["issue"], page=journal["page"], elocator=journal["elocator"])  # "stylers" are fstring lambdas
-    
-    def _publication_date(pub, epub, styler):   
+
+    def journal_meta(vol, iss, page, elocator, styler):
+        return styler(vol=vol, iss=iss, page=page, elocator=elocator)  # "stylers" are fstring template functions
+
+    def publication_date(pub, epub, styler):   
         return styler(pub=pub, epub=epub)
-    
-    def _identifiers(doi, pmid, pmcid, styler): 
+
+    def identifiers(doi, pmid, pmcid, styler): 
         return styler(doi=doi, pmid=pmid, pmcid=pmcid)
    
-    # Same interface wrappers around builder methods, associated with element type constants.
+    # Wrappers around builder methods for unified interface associated with element type constants.
     _COMPONENT_MAP = {
-        CE.TITLE:            lambda ref, conf: refdata["title"],  #_title(refdata["title"], **settings),
-        CE.AUTHORS:          lambda ref, conf: ElementBuilder._authors(refdata["authors"], **conf),
-        CE.JOURNAL_NAME:     lambda ref, conf: ElementBuilder._journal_name(refdata["journal"]["name"], **conf),
-        CE.JOURNAL_META:     lambda ref, conf: ElementBuilder._journal_meta(vol=refdata["journal"]["volume"], iss=refdata["journal"]["issue"], page=refdata["journal"]["page"], elocator=refdata["journal"]["elocator"], **conf),
-        CE.PUBLICATION_DATE: lambda ref, conf: ElementBuilder._publication_date(pub=refdata["pub"], epub=refdata["epub"], **conf),   # Might be cleaner to nest pub/epub under 'date' instead of flat. Same with identifiers? who cares
-        CE.IDENTIFIERS:      lambda ref, conf: ElementBuilder._identifiers(doi=refdata["doi"], pmid=refdata["pmid"], pmcid=refdata["pmcid"], **conf)
+        CitationElement.TITLE:            lambda ref, conf: ref["title"],  #_title(refdata["title"], **settings),
+        CitationElement.AUTHORS:          lambda ref, conf: ElementBuilder.authors(ref["authors"], **conf),
+        CitationElement.JOURNAL_NAME:     lambda ref, conf: ElementBuilder.journal_name(ref["journal"]["name"], **conf),
+        CitationElement.JOURNAL_META:     lambda ref, conf: ElementBuilder.journal_meta(vol=ref["journal"]["volume"], iss=ref["journal"]["issue"], page=ref["journal"]["page"], elocator=ref["journal"]["elocator"], **conf),
+        CitationElement.PUBLICATION_DATE: lambda ref, conf: ElementBuilder.publication_date(pub=ref["pub"], epub=ref["epub"], **conf),   # Might be cleaner to nest pub/epub under 'date' instead of flat. Same with identifiers? who cares
+        CitationElement.IDENTIFIERS:      lambda ref, conf: ElementBuilder.identifiers(doi=ref["doi"], pmid=ref["pmid"], pmcid=ref["pmcid"], **conf)
     }
    
     # Returns reference to lambdas which call builder functions.
@@ -104,43 +108,89 @@ class ElementBuilder:
         return cls._COMPONENT_MAP[element]
 
 class Formats:
-    class ComponentStylers:
-        def meta_vancouver(vol, iss, page, elocator):
-            styled = ""
-            if vol:
-                styled = str(vol)
-                if iss: 
-                    styled += f"({iss})"
-            if page["start"] or page["end"]:
-                styled += ":"
-                if page["start"]:
-                    styled += str(page["start"])
-                    if page["end"]: styled += "-" + str(page["end"])
-                else: styled += str(page["end"])
-            if elocator:
-                styled += f":{elocator}"
+    # Citations consist of the six CitationElements.
+    # ElementBuilder constructs elements, given the reference data source and a builder configuration dict.
 
-        def pub_vancouver(pub, epub):
+    # Formats uses ElementBuilder to create elements configured for a given format. 
+    # Formats.ElementStylers are used to customize some ElementBuilder components in place of a config dict. They're basically fstring templates, with some extra conditions for missing components.
+
+    # _LAYOUT defines where how and which CitationElements are included within a particular citation format.
+    # _STYLE_CONFIG defines the ElementBuilder configs and stylers to apply to each component for a particular format.
+
+    class ElementStylers:
+        # Helper for {y:2015 m:12 d:25} -> 2015 Dec 25
+        _MONTHS = { 1:  "Jan", 2:  "Feb", 3:  "Mar",
+                    4:  "Apr", 5:  "May", 6:  "Jun",
+                    7:  "Jul", 8:  "Aug", 9:  "Sep",
+                    10: "Oct", 11: "Nov", 12: "Dec" }
+        @classmethod
+        def _monthify_date(cls, date):
             styled = ""
-            date = pub if pub["y"] else epub
             if date["y"]:
                 styled = str(date["y"])
                 if date["m"]:
-                    styled += " " + MONTHS[date["m"]]
+                    styled += " " + cls._MONTHS[date["m"]]
                     if date["d"]:
                         styled += " " + str(date["d"])
             return styled
+        
+        # Vancouver Stylers
+        def vancouver_journal_meta(**metadata):
+            vol, iss, page, elocator = [metadata[part] for part in ("vol", "iss", "page", "elocator")]
+            styled = ""
+            styled += str(vol) if vol else ""
+            styled += f"({iss})" if iss else ""
+            if page["start"] and page["end"]:
+                styled += f":{page["start"]}-{page["end"]}"
+            elif page["start"] or page["end"]:
+                styled += f":{page["start"] if page["start"] else page["end"]}"
+            if elocator:
+                styled += f":{elocator}"
+            return styled
+        @classmethod
+        def vancouver_publication_date(cls, **pubdates):
+            pub, epub = [pubdates[p] for p in ("pub", "epub")]
+            date = pub if pub["y"] else epub
+            return cls._monthify_date(date)
+        def vancouver_identifiers(**idents):
+            doi = idents["doi"]
+            return f"Available from: doi:{doi["prefix"]}/{doi["suffix"]}"   # Maybe vancouver just shouldn't do DOI? Or it should do URLs instead? Variation needed...
+       
+        # APA Stylers
+        def apa_journal_meta(**metadata):
+            vol, iss, page, elocator = [metadata[part] for part in ("vol", "iss", "page", "elocator")]
+            styled = ""
+            styled += str(vol) if vol else ""
+            styled += f"({iss})" if iss else ""
+            if page["start"] and page["end"]:
+                styled += f", {page["start"]}-{page["end"]}"
+            elif page["start"] or page["end"]:
+                styled += f", {page["start"] if page["start"] else page["end"]}"
+            if elocator:
+                styled += f", {elocator}"
+            return styled
+        def apa_publication_date(**pubdates):
+            pub, epub = [pubdates[p] for p in ("pub", "epub")]
+            return f"({pub["y"]})" if pub["y"] else f"({epub["y"]})" if epub["y"] else None
+        def apa_identifiers(**idents):
+            doi = idents["doi"] # We've never encountered a reference without a DOI in the RIS. How would this break if it's absent?
+            return f"https://doi.org/{doi["prefix"]}/{doi["suffix"]}"
 
-    # Ordering/combining of citation elements
+    # Ordering/combining of rendered citation elements      # This is probabaly where the omission checking would come in?
+    def _layout_vancouver(authors, title, journal_name, publication_date, journal_meta, identifiers):   # ! Argument names should match CitationElement strings (so unwrapped CitationElement: "element" dicts can be used)
+        return f"{authors}. {title}. {journal_name}. {publication_date};{journal_meta}. {identifiers}"
+    def _layout_apa(authors, title, journal_name, publication_date, journal_meta, identifiers):
+        return f"{authors} {publication_date}. {title}. {journal_name}, {journal_meta}. {identifiers}"
     _LAYOUT = {
-        FS.VANCOUVER: lambda elements: f"{elements[CE.AUTHORS]}. {elements[CE.TITLE]}. {elements[CE.JOURNAL_NAME]}. {elements[CE.PUBLICATION_DATE]};{elements[CE.JOURNAL_META]}. {elements[CE.IDENTIFIERS]}"
+        FormatStyle.VANCOUVER: _layout_vancouver,
+        FormatStyle.APA: _layout_apa 
     }
 
     # Styling configuration of citation elements
     _STYLE_CONFIG = {
         # https://auckland.libguides.com/vancouver/journal-article
-        FS.VANCOUVER: {
-            CE.AUTHORS: {
+        FormatStyle.VANCOUVER: {
+            CitationElement.AUTHORS: {
                 "etal": ", et al",              # Include comma at start if desired.    ", et al" or " et al"
                 "etal_threshold": 6,
                 "delim_authors": ", ",
@@ -150,71 +200,44 @@ class Formats:
                 "initialize_periods": False,
                 "penultimate_and": "",          # Include comma at start for oxford comma. ", and " or " & " or ", & "  etc.
             },
-            CE.TITLE: {
-                None
+            CitationElement.TITLE:            { None },  # All supported styles use the default: sentence case.
+            CitationElement.JOURNAL_NAME:     { "abbreviate": True },
+            CitationElement.JOURNAL_META:     { "styler": ElementStylers.vancouver_journal_meta },
+            CitationElement.PUBLICATION_DATE: { "styler": ElementStylers.vancouver_publication_date },
+            CitationElement.IDENTIFIERS:      { "styler": ElementStylers.vancouver_identifiers }
+        },
+        FormatStyle.APA: {
+            CitationElement.AUTHORS: {
+                "etal": ", ... ",
+                "etal_apa": True,               # Appends ... <final_author> instead of an et al. 
+                "etal_threshold": 20,
+                "delim_authors": ", ",
+                "delim_initials": " ",
+                "delim_lastfirst": ", ",
+                "initialize": True,
+                "initialize_periods": True,
+                "penultimate_and": " & ",       # Include comma at start for oxford comma. ", and " or " & " or ", & "  etc.
+
             },
-            CE.JOURNAL_NAME: {
-                "abbreviate": True
-            },
-            CE.JOURNAL_META: {
-                "styler": ComponentStylers.meta_vancouver
-            },
-            CE.PUBLICATION_DATE: {
-                "styler": lambda pub, epub: Formats._Stylers.monthy_publication(pub) if pub["y"] else epub   # Would be better to have the actual element be null and not dict of "". So it goes.
-                #lambda pub, epub: f"{pub["y"]}{f" {MONTHS[pub["m"]]{f" {pub["d"]}" if pub["d"] else ""}}" if pub["m"] else ""}"
-            },
-            CE.IDENTIFIERS: {
-                "styler": lambda doi, pmid, pmcid: f"Available from: doi:{doi["suffix"]}/{doi["prefix"]}"   # Maybe vancouver just shouldn't do DOI? Or it should do URLs instead? Variation needed...
-            }
+            CitationElement.TITLE:            { None },
+            CitationElement.JOURNAL_NAME:     { "abbreviate": False },
+            CitationElement.JOURNAL_META:     { "styler": ElementStylers.apa_journal_meta },
+            CitationElement.PUBLICATION_DATE: { "styler": ElementStylers.apa_publication_date },
+            CitationElement.IDENTIFIERS:      { "styler": ElementStylers.apa_identifiers }
         }
     }
    
     @classmethod
     def build(cls, refdata, style):
-        elements = {ce: cls._ElementBuilder.build(ce)(refdata, cls._STYLE_CONFIG[style][ce]) for ce in CE}    # @TODO factor the omissions into this.
-        print(cls._LAYOUT[style](elements))
-        
-    #_COMPONENT_PROCESSING = {
-    #    RC.AUTHORS:             Formats._authorize
-    #    RC.TITLE:               lambda title: title
-    #    RC.JOURNAL_NAME:        lambda 
-    #    RC.JOURNAL_VOLUME:  
-    #    RC.JOURNAL_ISSUE:   
-    #    RC.JOURNAL_PAGE:    
-    #    RC.ELOCATOR:        
-    #    RC.PUBLICATION_DATE:
-    #    RC.DOI:             
-    #    RC.PMID:            
-    #    RC.PMCID:           
-    #}
+        elements = {element: ElementBuilder.build(element)(refdata, cls._STYLE_CONFIG[style][element]) for element in CitationElement}    # @TODO factor the omissions into this.
+        print(cls._LAYOUT[style](**elements))
 
-
-#    @classmethod
-#    def build(cls, refdata, style):    # components: list[ReferenceComponent]
-#        # Combine given components in sequential order
-#        _COMPONENT_PROCESSING = {
-#            RC.AUTHORS:             lambda rd, s: cls._authorize(refdata["authors"], **cls._STYLE_CONFIGS[s][RC.AUTHORS]),
-#            RC.TITLE:               lambda rd, s: rd["title"], # Special processing?
-#            RC.JOURNAL_NAME:        lambda rd, s: rd["journal"]["name"][cls._STYLE_CONFIGS[s][RC.JOURNAL_NAME]],
-#            RC.JOURNAL_VOLUME:      lambda rd, s: rd["journal"]["volume"],  # Special styling ? (Paranthesis, semicolons and stuff?)
-#            RC.JOURNAL_ISSUE:       lambda rd, s: rd["journal"]["issue"],
-#            RC.JOURNAL_PAGE:        lambda rd, s: str(rd["journal"]["page"]["start"]) + "-" + str(rd["journal"]["page"]["end"]),
-#            RC.ELOCATOR:            lambda rd, s: rd["journal"]["elocator"],
-#            RC.PUBLICATION_DATE:    lambda rd, s: rd["pub"], # or rd["epub"] ? Hopefully deteremined by style. As well as y/m/d.
-#            RC.DOI:                 lambda rd, s: rd["doi"]["prefix"] + "/" + rd["doi"]["suffix"], # Style should determine https:// inclusion etc.
-#            RC.PMID:                lambda rd, s: rd["pmid"],
-#            RC.PMCID:               lambda rd, s: rd["pmcid"]
-#        }
-#
-#        for c in _COMPONENT_PROCESSING:
-#            print(_COMPONENT_PROCESSING[c](refdata, style))
-
-import h
 if __name__ == "__main__":
-    pass
+    import h
     refs = h.read_json("refset.json", intkeys=True)
 #    Formats.build(refs[88]["data"], FormatStyle.VANCOUVER)
     Formats.build(refs[88]["data"], FormatStyle.VANCOUVER)
+    Formats.build(refs[88]["data"], FormatStyle.APA)
     #Formats._authorize(ass, **Formats._STYLE_CONFIGS[FormatStyle.VANCOUVER]["authors"])
 
 """
